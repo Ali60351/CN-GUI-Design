e298e7fd0b9fe643eef328a484a977df1aac6d87
Ali Nauman <ali60351@gmail.com>
2017-11-12 03:33:58 +0500

    Dynamically drew graph

--- Program.js
+++ Program.js
@@ -2,8 +2,29 @@
 function draw()
 {
     /* eslint-enable */
-    var Meow = 50;
+    var Name = "Meow";
+    var Value = 50;
 
-    $("#MeowValue").css("width", String((85 / 100) * Meow) + "%");
-    $("#MeowPercentage").html(String(Meow) + "%");
+    var graphDiv = $("<div/>", {"class": "graph"});
+    var graphBarDiv = $("<div/>", {"class": "graph-bar"});
+
+    var graphLabelDiv = $("<div/>", {"class": "graph-label"});
+    var graphLabel = $("<p>" + Name + "</p>");
+    graphLabelDiv.html(graphLabel);
+
+    var graphBar = $("<div/>", {"class": "graph-value", "id": Name + "Value"});
+
+    var graphPrecentageDiv = $("<div/>", {"class": "graph-percentage"});
+    var graphPercentage = $("<p>" + String(Value) + "%" + "</p>").attr("id", Name + "Percentage");
+    graphPrecentageDiv.html(graphPercentage);
+
+    graphBarDiv.append(graphLabelDiv);
+    graphBarDiv.append(graphBar);
+    graphBarDiv.append(graphPrecentageDiv);
+
+    graphDiv.html(graphBarDiv);
+    $("#target").html(graphDiv);
+
+    $("#MeowValue").css("width", String((85 / 100) * Value) + "%");
+    $("#MeowPercentage").html(String(Value) + "%");
 }
